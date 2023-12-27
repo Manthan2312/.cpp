cd94f69b34c404f047036b4144a7c1654e3ef5f5
Manthan2312 <145533317+Manthan2312@users.noreply.github.com>
2023-12-27 15:39:19 +0530

    Add files via upload

---
+++ constant and setw.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+       const int a=23;
+            double x=45.32;
+
+        cout<<a<<endl;
+        cout<<a<<endl;
+
+        cout<<"value:"<<setw(10)<<x<<endl;
+
+        
+}
