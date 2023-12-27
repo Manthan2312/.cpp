bae39ad401a758bbb038d99c579c4770f29d8777
Manthan2312 <145533317+Manthan2312@users.noreply.github.com>
2023-12-27 15:37:39 +0530

    Add files via upload

---
+++ VARIABLE.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+int a=111;
+void man()
+{
+   cout<<"MANTHAN PATEL\n";
+   cout<<a;
+}
+int main()
+{
+    int a=1,b=86;
+    char u='m';
+    float x=1.43,y=32.34;
+    double m=3.2222345;
+    bool k=true,n=false;
+
+    cout<<"types of data\n";
+    cout<<a<<b;
+    cout<<u;
+    cout<<x<<y;
+    cout<<m;
+    man();
+    cout<<k<<n;
+    return 0;
+}
