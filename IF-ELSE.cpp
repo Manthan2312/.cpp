204ce0ea94a2c91e834c550e3ffc2997566d3508
Manthan2312 <145533317+Manthan2312@users.noreply.github.com>
2023-12-27 15:40:05 +0530

    Add files via upload

---
+++ IF-ELSE.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter your number here"<<endl;
+    cin>>a;
+
+    if(a==0)
+    {
+        cout<<"this is zero"<<endl;
+    }
+    else if (a/2==1)
+    {
+    cout<<a<<"is a even number"<<endl;
+    }
+    else
+   {
+    cout<<a<<"is a odd number"<<endl;
+   }
+return 0;
+
+}
