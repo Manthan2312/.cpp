2a56fa4f13e38f17efffd36cfa8fcbd580e4c1b8
Manthan2312 <145533317+Manthan2312@users.noreply.github.com>
2023-12-27 15:42:01 +0530

    Add files via upload

---
+++ LOGICAL OP.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout<<"ENTER YOUR VALUES"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+
+    if ((a>b)&&(a>c))
+    {
+        cout<<a<<"is big number"<<endl;
+    }
+    else if((b>a)&&(b>c))
+    {
+        cout<<b<<"is big number"<<endl;
+    }
+    else if((a==b)&&(b==c))
+    {
+        cout<<"all the values are same"<<endl;
+    }
+    else
+    {
+        cout<<c<<"is big number"<<endl;
+    }
+
+    return 0;
+}
