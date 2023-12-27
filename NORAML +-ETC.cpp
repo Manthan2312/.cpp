3b4ec7a2f5c5be8f4a3439c3bab2f2bcd7102a0d
Manthan2312 <145533317+Manthan2312@users.noreply.github.com>
2023-12-27 15:36:11 +0530

    Add files via upload

---
+++ NORAML +-ETC.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int num1,num2;
+
+    cout<<"ENTER YOUR FIRST VALUE\n";
+    cin>>num1;
+
+    cout<<"ENTER YOUR SECOND VALUE\n";
+    cin>>num2;
+
+    cout<<"addition:"<<num1+num2;
+    cout<<"subtration:"<<num1-num2;
+    cout<<"multiplicaton:"<<num1*num2;
+    cout<<"divison:"<<num1/num2;
+    cout<<"modulo:"<<num1%num2;
+    return 0;
+}
